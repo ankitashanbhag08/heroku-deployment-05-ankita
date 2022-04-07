6a6b8ee2f97114e0c0c56f227207681e6774a917
Ankita Shanbhag <ankita.shanbhag@tuni.fi>
2022-04-07 11:54:07 +0300

    Add: Code to access database.

---
+++ crudrepository.js
@@ -0,0 +1,41 @@
+require('dotenv').config()
+const mysql = require('mysql')
+
+var connection = mysql.createConnection({ 
+ host: process.env.DB_HOST,
+ user: process.env.DB_USER,
+ password: process.env.DB_PASSWORD,
+ database: process.env.DB_DB,
+ multipleStatements: false
+});
+
+const fieldNames=["latitude", "longitude"]
+
+let connectionFunctions = {
+    connect:(callback)=>{
+        connection.connect((err)=>{
+            callback();
+        });
+    },
+    findAll:(filters)=>{
+        let sql='SELECT * FROM locations '
+        
+        return new Promise((resolve, reject)=>{
+                connection.query(sql, (err, results)=>{
+                err ? reject(err) : resolve(results)
+            })
+        })
+        
+    },
+    
+    
+    
+    close: (callback)=>{
+        connection.end(()=>{
+            console.log("Closing db")
+            callback();
+        })
+    }
+}
+
+module.exports = connectionFunctions
--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-
+require('dotenv').config()
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 
 const db = [{ name: "tiina" }, { name: "jack1" }];
+const database = require('./crudrepository')
 
 app.get("/names", (req, res) => {
   res.send(db);
+});
+
+app.get("/locations", async (req, resp)=>{
+    try{
+        const filters=req.query;
+        const results = await database.findAll(filters);
+        resp.send(results);
+    }catch(err){
+        resp.status(500).end();
+    }    
 });
 
 const server = app.listen(port, () => {
